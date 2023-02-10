1aabb913a86c364193034a63e0ee7275b1f33d24
maixuany <maixuanytp3@gmail.com>
2023-02-10 19:06:39 +0700

    handlebars

--- index.js
+++
@@ -1,6 +0,0 @@
-const expess = require('express')
-const app = expess()
-
-const port = 3000
-app.get('/tin-tuc', (req, res)=> res.send('Hello World'))
-app.listen(port, ()=>console.log(`Example app listening at http://localhost:${port}`))
